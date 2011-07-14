ba42f009267ab1185af257f981bf8af1eae16b93
dogmatic69 <dogmatic69@gmail.com>
2011-07-14 20:58:35 +0100

    adding an example of how you might call methods in other classes normally

---
+++ example-1.php
@@ -0,0 +1,104 @@
+<?php
+	############################################################################
+	# Bootstrap - load up classes and configs etc
+	############################################################################
+	function pr($var){
+		echo '<pre>'; print_r($var); echo '</pre>';
+	}
+
+	/**
+	 * base class
+	 */
+	class Controller {
+		public $controllers = array('BlogsController', 'UsersController', 'OtherController');
+		public function output($array){
+			return sprintf('<ul><li>%s</li></ul>', implode('</li><li>', (array)$array));
+		}
+	}
+
+	############################################################################
+	# Your cool code (components / models etc
+	############################################################################
+
+	/**
+	 * some blog stuff
+	 */
+	class BlogsController extends Controller {
+		/**
+		 * call the onHelloWorld method in all classes
+		 */
+		public function view(){
+			echo sprintf('<h1>%s - view</h1>', __CLASS__);
+
+			echo '<h3>calling methods in other classes</h3>';
+			$return = array();
+			foreach($this->controllers as $controller){
+				$Controller = new $controller();
+				$return[] = $Controller->onHelloWorld('');
+			}
+
+			if(!empty($return)){
+				echo $this->output($return);
+			}
+		}
+
+		public function onHelloWorld($data = null){
+			return 'Blogs Controller Here';
+		}
+	}
+
+	/**
+	 * some user stuff
+	 */
+	class UsersController extends Controller {
+		/**
+		 * call the method only in the blogs controller
+		 */
+		public function view(){
+			echo sprintf('<h1>%s - view</h1>', __CLASS__);
+
+			echo '<h3>Only BlogsController when its around</h3>';
+			$Controller = new BlogsController();
+			$return = array();
+			$return = $Controller->onHelloWorld('');
+
+			if(!empty($return)){
+				echo $this->output($return);
+			}
+		}
+
+		public function onHelloWorld($data = null){
+			return 'Users Controller Here';
+		}
+	}
+
+	/**
+	 * some other controller
+	 */
+	class OtherController extends Controller {
+		/**
+		 * here we trigger an event to any class that has a 'helloWorld' event
+		 */
+		public function view(){
+			echo sprintf('<h1>%s - view</h1>', __CLASS__);
+
+			echo '<h3>nothing to see here, move along</h3>';
+		}
+
+		public function onHelloWorld($data = null){
+			return 'Other Controller Here';
+		}
+	}
+
+	############################################################################
+	# Dispacher - process the request and run it
+	############################################################################
+
+	/**
+	 * do some actual calls
+	 */
+	$Blog = new BlogsController();
+	$Blog->view();
+
+	$User = new UsersController();
+	$User->view();
